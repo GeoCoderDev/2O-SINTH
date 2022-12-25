45b909b9fc5af8c7bd1585dda02ef91eb180166c
Juanito <juanchavezsaldana1@gmail.com>
2022-12-25 17:02:25 -0500

    Se configuro todo el funcionamiento de la barra de navegacion

---
+++ Assets/JavaScript/Observadores.js
@@ -0,0 +1,33 @@
+const seccion_sintetizador = document.getElementById('contenedor-sintetizador');
+const seccion_efectos = document.getElementById('contenedor-efectos'); 
+const seccion_secuenciador_melodias = document.getElementById('contenedor-secuenciador-melodias');
+const seccion_secuenciador_de_ritmos = document.getElementById('contenedor-secuenciador-ritmos');
+var seccion_en_vista;
+
+
+
+const encontrando_secciones = (entradas, observador) => {
+    
+    entradas.forEach((entrada) => {
+        if (entrada.isIntersecting){
+            seccion_en_vista = entrada.target.dataset.numero_seccion;
+            var indicador = document.getElementById('indicador');
+            indicador.style.top = (32.5 + ((seccion_en_vista-1)*12.9)) + "vh";
+            indicador.style.marginBottom = (55 - ((seccion_en_vista-1)*12.9)) + "vh";
+        }
+    });    
+};
+
+
+
+const Observador_de_Secciones = new IntersectionObserver(encontrando_secciones,{
+    root: null,
+    rootMargin: '0px 0px 0px 0px',
+    threshold: 1.0    
+});
+
+Observador_de_Secciones.observe(seccion_sintetizador);
+Observador_de_Secciones.observe(seccion_efectos);
+Observador_de_Secciones.observe(seccion_secuenciador_melodias);
+Observador_de_Secciones.observe(seccion_secuenciador_de_ritmos);
+
