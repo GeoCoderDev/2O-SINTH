60b79e33345d194d8cb6dbcd71d1eb4c8af528d8
Juanito3500 <juanchavezsaldana1@gmail.com>
2023-12-17 20:03:55 -0500

    Probando formulario

--- Assets/JavaScript/Login-Register-Scripts.js
+++ Assets/JavaScript/Login-Register-Scripts.js
@@ -26,9 +26,16 @@ LOGIN_FORM.addEventListener("submit", (e)=>{
     
     e.preventDefault();
 
+    // Redirigiendo a 2O-SINTH
     let urlActual = new URL(window.location.href);
 
     window.location.href = urlActual.host + "/2O-SINTH";
 
+        // Accede a diferentes partes de la URL
+        console.log('Protocolo:', urlActual.protocol);
+        console.log('Host:', urlActual.host);
+        console.log('Ruta:', urlActual.pathname);
+        console.log('Parámetros de búsqueda:', urlActual.search);
+        console.log('Hash:', urlActual.hash);
 })
 
