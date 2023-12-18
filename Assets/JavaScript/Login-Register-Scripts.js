eae6439b8e1f0b237ae8610d51043b1baca4551e
Juanito3500 <juanchavezsaldana1@gmail.com>
2023-12-17 20:20:26 -0500

    Probando formulario

--- Assets/JavaScript/Login-Register-Scripts.js
+++ Assets/JavaScript/Login-Register-Scripts.js
@@ -1,20 +1,18 @@
-
 // ========================================================================
 // |          FUNCIONALIDADES NECESARIAS PARA EL FORMULARIO DOBLE         |
 // ========================================================================
 
 const wrapper = document.querySelector(".wrapper");
-const loginLink = document.querySelector(".login-link")
+const loginLink = document.querySelector(".login-link");
 const registerLink = document.querySelector(".register-link");
 
-registerLink.addEventListener("click",()=>{
-    wrapper.classList.add("register-mode");
-})
+registerLink.addEventListener("click", () => {
+  wrapper.classList.add("register-mode");
+});
 
-loginLink.addEventListener("click",()=>{
-    wrapper.classList.remove("register-mode");
-})
-
+loginLink.addEventListener("click", () => {
+  wrapper.classList.remove("register-mode");
+});
 
 // =======================================================================
 // |                              LOGIN                                  |
@@ -22,15 +20,17 @@ loginLink.addEventListener("click",()=>{
 
 const LOGIN_FORM = document.getElementById("Login-Form");
 
-LOGIN_FORM.addEventListener("submit", (e)=>{
-    
-    e.preventDefault();
-    
-    // Redirigiendo a 2O-SINTH
-    window.location.href = "";
+LOGIN_FORM.addEventListener("submit", (e) => {
+  e.preventDefault();
 
 
+  // Obtiene la URL actual
+  let urlActual = new URL(window.location.href);
 
+  // Obtiene la URL base (eliminando la ruta y el nombre del archivo)
+  let urlBase = urlActual.protocol + "//" + urlActual.host;
 
-})
+  // Redirige a la URL base
+  window.location.href = urlBase;
 
+});
